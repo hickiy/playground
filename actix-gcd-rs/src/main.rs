d301e2a674b3bcaa3006bfef3bc465f8614eeacc
hickey <596310898@163.com>
2024-08-11 23:13:02 +0800

    actix-gcd-rs: correctly run the demo

--- actix-gcd-rs/src/main.rs
+++ actix-gcd-rs/src/main.rs
@@ -1,13 +1,75 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+use std::process::exit;
+use signal_hook::{consts::signal::SIGHUP, iterator::Signals};
+
 fn main() {
-    let server = HttpServer::new(|| App::new().route("/", web::get().to(get_index)));
-    println!("Server running at http://localhost:8080");
+    let mut signals = Signals::new(&[SIGHUP]).expect("Error registering signal handler");
+    std::thread::spawn(move || {
+        for sig in signals.forever() {
+           match sig {
+            _sighup => {
+                   println!("Received SIGHUP, exiting...");
+                   exit(0);
+               }
+           }
+        }
+    });
+
+    let server = HttpServer::new(|| 
+        App::new()
+        .route("/", web::get().to(get_index))
+        .route("/gcd", web::post().to(post_gcd)));
+    println!("Serving on http://localhost:3000...");
     server
-        .bind("127.0.0.1:8080")
-        .expect("Can not bind to port 8080")
+        .bind("127.0.0.1:3000")
+        .expect("error binding server to address")
         .run()
+        .expect("error running server");
 }
 
-fn get_index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+fn get_index() -> HttpResponse {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+        <title>GCD Calculator</title>
+        <form action="/gcd" method="post">
+        <input type="text" name="n"/>
+        <input type="text" name="m"/>
+        <button type="submit">Compute GCD</button>
+        </form>
+        "#,
+    )
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+    HttpResponse::Ok().content_type("text/html").body(format!(
+        "The greatest common divisor of the numbers {} and {} is <b>{}</b>",
+        form.n,
+        form.m,
+        gcd(form.n, form.m)
+    ))
 }
