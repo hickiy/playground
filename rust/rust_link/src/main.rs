6290f62e9bd26e2b8763310aa293504cecb19bf7
hickey <596310898@163.com>
2025-08-18 15:42:13 +0800

    rust: improve rust_link demo

--- rust/rust_link/src/main.rs
+++ rust/rust_link/src/main.rs
@@ -8,25 +8,9 @@ extern "C" {
     fn hello2();
 }
 
-use libloading::{ Library, Symbol };
-use std::path::PathBuf;
-use std::env;
-
 fn main() {
     unsafe {
         hello1();
-        hello2();
-    }
-    // 构建动态库的相对路径
-    let mut lib_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let out_dir = env::var("OUT_DIR").unwrap();
-    lib_path.push(out_dir + r"\hello2.dll");
-    // 获取函数指针
-    unsafe {
-        // 加载动态库
-        let lib = Library::new(lib_path).unwrap();
-        let hello2: Symbol<unsafe extern "C" fn()> = lib.get(b"hello2").unwrap();
-        // 调用函数
         hello2();
     }
 }
